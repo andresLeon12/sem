efc2a919b68f954c88337bdc5e57cd0d80908376
Andres <landres697@gmail.com>
2016-01-07 01:06:12 -0600

    Notificaciones

--- js/app_perfil.js
+++ js/app_perfil.js
@@ -1,38 +1,26 @@
-/* Controlador para secretario */
+/* Controlador para Actualizar el Perfil */
 var app = angular.module('perfil', [])
-var url_server = 'http://159.203.128.165:8080/';
+var url_server = 'http://127.0.0.1:8080/';
 
 app.controller('perfilController', function($scope, $http){
-
+    console.log("Esto Contiene localStorage:"+localStorage.getItem("empresa_server"));
     var empresa = localStorage.getItem("empresa_server");
     $scope.empresa = JSON.parse(empresa);
+		console.log($scope.empresa);
 
     $scope.updateEmpresa = function() {
         var empresa = $scope.empresa;
-        var temporal = empresa._id;
         $('#'+empresa._id+"-Update").closeModal();
         empresa.id = empresa._id; // Pasamos la _id a id para mayor comodidad del lado del servidor a manejar el dato.
-        delete empresa._id; // Lo borramos para evitar posibles intentos de modificación de un ID en la base de datos
-        // Hacemos una petición PUT para hacer el update a un documento de la base de datos.
-        //var nombre_puesto = $("#puesto_empleado_update-"+usuario.id+" option:selected").text();
-        //usuario.puesto_nombre = nombre_puesto;
+        //delete empresa._id; //
         $http.put(url_server+"empresa/actualizar", empresa).success(function(response) {
             if(response.status === "OK") {
-                //localStorage.setItem("empresa_server") = response.data
-                getEmpresa(temporal); // Actualizamos la lista de ToDo's
+                localStorage.empresa_server = JSON.stringify(empresa);
                 $("#mensaje").empty();
                 $("#mensaje").append('<div class="chip">Información actualizada <i class="material-icons">Cerrar</i></div>');
                 $("#mensaje").css('color', '#FFF');
             }
-        })
-    }
-
-    /* Método para obtener información de un usuario específico */
-    function getEmpresa(a) {
-        $http.get(url_server+"empresa/buscar/"+a).success(function(response) {
-            if(response.type) { // Si nos devuelve un OK la API...
-                $scope.empresa = response.data[0];
-            }
         });
     }
+
 });
